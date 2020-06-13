799b17bb51b158b612320a6907ace3c1dba3d881
PaulHiggs <paul_higgs@hotmail.com>
2020-06-13 08:28:36 +0100

    initial commit

---
+++ ErrorList.js
@@ -0,0 +1,30 @@
+/**
+ * Manages errors and warnings for the application
+ * 
+ */
+module.exports = class ErrorList {
+    counts=[]; messages=[]; countsWarn=[]; messagesWarn=[];
+    
+    increment(key) {
+        if (this.counts[key]===undefined)
+            this.set(key,1);
+        else this.counts[key]++;
+    }
+    set(key,value) {
+        this.counts[key]=value;
+    }
+    incrementW(key) {
+        if (this.countsWarn[key]===undefined)
+            this.setW(key,1);
+        else this.countsWarn[key]++;
+    }
+    setW(key,value) {
+        this.countsWarn[key]=value;
+    }
+    push(message) {
+        this.messages.push(message);
+    }
+    pushW(message) {
+        this.messagesWarn.push(message);
+    }
+}
