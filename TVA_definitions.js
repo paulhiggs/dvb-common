3dc938d40ae24ffe61f983b8671d71697ec3581f
PaulHiggs <paul_higgs@hotmail.com>
2020-06-24 21:36:26 +0100

    TVA constants

---
+++ TVA_definitions.js
@@ -0,0 +1,23 @@
+
+module.exports = Object.freeze({
+
+	e_RelatedMaterial: 	"RelatedMaterial",
+	e_HowRelated: 		"HowRelated",
+	e_Synopsis:			"Synopsis",
+	e_Keyword:			"Keyword",
+	e_Genre:			"Genre",
+	e_ParentalGuidance:	"ParentalGuidance",
+	e_MinimumAge:		"MinimumAge",
+	e_ParentalRating:	"ParentalRating",
+	e_ExplanatoryText:	"ExplanatoryText",
+	e_GivenName:		"GivenName",
+	e_FamilyName:		"FamilyName",
+	
+	a_length:			"length",
+	v_lengthLong:		"long",
+	
+	a_type:				"type",
+	a_href:				"href",
+	
+	__zzzTVAENDzzz__: null
+});
