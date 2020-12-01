9e0ec8f38555508fa694a61aaaaecd569af9b615
PaulHiggs <paul_higgs@hotmail.com>
2020-12-01 16:43:18 +0000

    allow loading of leaf terms only

--- CS_handler.js
+++ CS_handler.js
@@ -1,7 +1,23 @@
 //---------------- CLASSIFICATION SCHEME LOADING ---------------- 
 
-const fs=require("fs");
-const libxml = require("libxmljs");
+const fs=require("fs")
+const libxml = require("libxmljs")
+
+/**
+ * check if the element contains the named child element
+ *
+ * @param {Object} elem the element to check
+ * @param {string} childElementName the name of the child element to look for
+ *& @returns {boolean} true of the element contains the named child element(s) otherwise false
+ */
+function hasChild(elem, childElementName) {
+	let ch=0, c
+	while (c=elem.child(ch))
+		if (c.type()=="element" && c.name()==="Term")
+			return true
+	
+	return false
+}
 
 /**
  * Constructs a linear list of terms from a heirarical clssification schemes which are read from an XML document and parsed by libxmljs
@@ -9,13 +25,16 @@ const libxml = require("libxmljs");
  * @param {Array} values The array to push classification scheme values into
  * @param {String} CSuri The classification scheme domian
  * @param {Object} term The classification scheme term that may include nested subterms
+ * @param {boolean} leafNodesOnly flag to indicate if only the leaf <term> values are to be loaded 
  */
-function addCSTerm(values, CSuri, term){
+function addCSTerm(values, CSuri, term, leafNodesOnly=false) {
     if (term.name()==="Term") {
-        values.push(CSuri+":"+term.attr("termID").value())
-        var st=0, subTerm;
+		if (leafNodesOnly && !hasChild(term, "Term"))
+			if (term.attr("termID")) 
+				values.push(CSuri+":"+term.attr("termID").value())
+        var st=0, subTerm
         while (subTerm=term.child(st++))
-            addCSTerm(values,CSuri,subTerm);
+            addCSTerm(values, CSuri, subTerm, leafNodesOnly)
     }
 }
 
@@ -24,14 +43,15 @@ function addCSTerm(values, CSuri, term){
  *
  * @param {Array} values The linear list of values within the classification scheme
  * @param {String} xmlCS the XML document  of the classification scheme
+ * @param {boolean} leafNodesOnly flag to indicate if only the leaf <term> values are to be loaded 
  */
-function loadClassificationScheme(values, xmlCS) {
-	if (!xmlCS) return;
-	var CSnamespace = xmlCS.root().attr("uri");
-	if (!CSnamespace) return;
-	var t=0, term;
+function loadClassificationScheme(values, xmlCS, leafNodesOnly=false) {
+	if (!xmlCS) return
+	let CSnamespace = xmlCS.root().attr("uri")
+	if (!CSnamespace) return
+	var t=0, term
 	while (term=xmlCS.root().child(t++))
-		addCSTerm(values,CSnamespace.value(),term);
+		addCSTerm(values, CSnamespace.value(), term, leafNodesOnly)
 }
 
 /**
@@ -39,12 +59,13 @@ function loadClassificationScheme(values, xmlCS) {
  *
  * @param {Array} values The linear list of values within the classification scheme
  * @param {String} classificationScheme the filename of the classification scheme
+ * @param {boolean} leafNodesOnly flag to indicate if only the leaf <term> values are to be loaded 
  */
-function loadCSfromFile(values, classificationScheme) {
+function loadCSfromFile(values, classificationScheme, leafNodesOnly=false) {
 	console.log("reading CS from", classificationScheme);
     fs.readFile(classificationScheme, {encoding: "utf-8"}, function(err,data){
         if (!err) 
-			loadClassificationScheme(values, libxml.parseXmlString(data.replace(/(\r\n|\n|\r|\t)/gm,"")));
+			loadClassificationScheme(values, libxml.parseXmlString(data.replace(/(\r\n|\n|\r|\t)/gm,"")), leafNodesOnly);
 		else console.log(err);
     });
 }
@@ -54,14 +75,15 @@ function loadCSfromFile(values, classificationScheme) {
  *
  * @param {Array} values The linear list of values within the classification scheme
  * @param {String} csURL URL to the classification scheme
+ * @param {boolean} leafNodesOnly flag to indicate if only the leaf <term> values are to be loaded 
  */
-function loadCSfromURL(values, csURL) { 
+function loadCSfromURL(values, csURL, leafNodesOnly=false) { 
 	console.log("retrieving CS from", csURL);
 	var xhttp = new XmlHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4) {
 			if (this.status == 200) 
-				loadClassificationScheme(values, libxml.parseXmlString(xhttp.responseText));
+				loadClassificationScheme(values, libxml.parseXmlString(xhttp.responseText), leafNodesOnly);
 			else console.log("error ("+this.status+") retrieving "+csURL);	
 		}
 	};
@@ -72,15 +94,15 @@ function loadCSfromURL(values, csURL) {
 /**
  * loads classification scheme values from either a local file or an URL based location
  *
- * @param {Array} values The linear list of values within the classification scheme
- * @param {boolean} useURL if true use the URL loading method else use the local file
- * @param {String} CSfilename the filename of the classification scheme
- * @param {String} CSurl URL to the classification scheme
- * 
+ * @param {Array} values          The linear list of values within the classification scheme
+ * @param {boolean} useURL        if true use the URL loading method else use the local file
+ * @param {String} CSfilename     the filename of the classification scheme
+ * @param {String} CSurl          URL to the classification scheme
+ * @param {boolean} leafNodesOnly flag to indicate if only the leaf <term> values are to be loaded 
  */ 
-module.exports.loadCS = function (values, useURL, CSfilename, CSurl) {
+module.exports.loadCS = function (values, useURL, CSfilename, CSurl, leafNodesOnly=false) {
 	if (useURL)
-		loadCSfromURL(values,CSurl);
-	else loadCSfromFile(values, CSfilename);	
+		loadCSfromURL(values, CSurl, leafNodesOnly)
+	else loadCSfromFile(values, CSfilename, leafNodesOnly)
 } 
 //--------------------------------------------------------------- 
