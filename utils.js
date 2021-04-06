986888637868413e20a07a69d8f0656d62a446f8
PaulHiggs <paul_higgs@hotmail.com>
2021-04-06 14:55:31 +0100

    add parseIDOduration() and allow common code for isIn() and isIni()

--- utils.js
+++ utils.js
@@ -30,6 +30,17 @@ module.exports.xPathM = function (SCHEMA_PREFIX, elementNames) {
 };
 
 
+/* local */ function findInSet(values, value, caseSensitive) {
+	let vlc=value.toLowerCase();
+    if (typeof(values)=="string")
+        return caseSensitive? values==value : values.toLowerCase()==vlc;
+
+    if (typeof(values)=="object")
+		return caseSensitive? values.includes(value) : (values.find(element => element.toLowerCase()==vlc) != undefined);
+
+    return false;
+}
+
 /**
  * determines if a value is in a set of values 
  *
@@ -38,14 +49,7 @@ module.exports.xPathM = function (SCHEMA_PREFIX, elementNames) {
  * @return {boolean} if value is in the set of values
  */
 module.exports.isIn = function(values, value, caseSensitive=true){
-	let vlc=value.toLowerCase();
-    if (typeof(values)=="string")
-        return caseSensitive? values==value : values.toLowerCase()==vlc;
-   
-    if (typeof(values)=="object") 	
-		return caseSensitive? values.includes(value) : (values.find(element => element.toLowerCase()==vlc) != undefined);
-    
-    return false;
+	return findInSet(values, value, caseSensitive);
 };
 
 
@@ -57,7 +61,7 @@ module.exports.isIn = function(values, value, caseSensitive=true){
  * @return {boolean} if value is in the set of values
  */
 module.exports.isIni = function(values, value){
-	return isIn(values, value, false);
+	return findInSet(values, value, false);
 };
 
 
@@ -69,4 +73,44 @@ module.exports.isIni = function(values, value){
  */
 module.exports.unEntity = function(str) {
 	return str.replace(/(&.+;)/ig, "*");
+};
+
+
+
+
+// credit to https://gist.github.com/adriengibrat/e0b6d16cdd8c584392d8#file-parseduration-es5-js
+module.exports.parseISOduration = function(duration) {
+	var durationRegex = /^(-)?P(?:(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)D)?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?)?|(\d+)W)$/;
+	var parsed;
+	if (duration) duration.replace(durationRegex, function (_, sign, year, month, day, hour, minute, second, week) {
+		sign = sign ? -1 : 1;
+		// parse number for each unit
+		var units = [year, month, day, hour, minute, second, week].map(function (num) { return parseInt(num, 10) * sign || 0; });
+		parsed = {year: units[0], month: units[1], week: units[6], day: units[2], hour: units[3], minute: units[4], second: units[5]};
+	} );
+	// no regexp match
+	if (!parsed) { throw new Error('Invalid duration "' + duration + '"'); }
+	/**
+	 * Sum or substract parsed duration to date
+	 *
+	 * @param {Date} date: A valid date instance
+	 * @throws {TypeError} When date is not valid
+	 * @returns {Date} Date plus or minus duration, according duration sign
+	 */
+	parsed.add = function add (date) {
+		if (Object.prototype.toString.call(date) !== '[object Date]' || isNaN(date.valueOf())) {
+			throw new TypeError('Invalid date');
+		}
+		return new Date(Date.UTC(
+			date.getUTCFullYear() + parsed.year,
+			date.getUTCMonth() + parsed.month,
+			date.getUTCDate() + parsed.day + parsed.week * 7,
+			date.getUTCHours() + parsed.hour,
+			date.getUTCMinutes() + parsed.minute,
+			date.getUTCSeconds() + parsed.second,
+			date.getUTCMilliseconds()
+		));
+	};
+
+	return parsed;
 };
